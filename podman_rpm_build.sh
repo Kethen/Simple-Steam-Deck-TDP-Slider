4222c60fcf3df96767d047e55aa3177eb056f354
Katharine Chui <kwchuiaa@connect.ust.hk>
2023-08-08 20:17:23 +0800

    update podman build script

--- podman_rpm_build.sh
+++ podman_rpm_build.sh
@@ -5,7 +5,7 @@ podman run \
 	tumbleweed \
 	bash -c '
 
-zypper -n install cargo cmake gcc-c++ fontconfig-devel
+zypper -n install cargo cmake gcc-c++ fontconfig-devel rpm-build
 
 cp /workdir/rpmmacros ~/.rpmmacros
 rpmbuild -bb deck-tdp-slider.spec
